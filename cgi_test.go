690ff14ba88aa288e496fcc9dbffbedb02086e41
Andreas Schneider <aksdb@gmx.de>
2020-08-01 11:50:20 +0200

    ✅ Add integration tests

--- cgi_test.go
+++ cgi_test.go
@@ -1,369 +1,137 @@
-//+build ignore
-
 package cgi
 
 import (
-	"bytes"
-	"fmt"
+	"context"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"runtime"
+	"reflect"
 	"strings"
 	"testing"
 
-	"github.com/caddyserver/caddy"
-	"github.com/caddyserver/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 )
 
-// handleGet returns a cgi handler (which implements ServeHTTP) based on the
-// specified directive string. The directive can contain more than one block,
-// but only the first is associated with the returned handler.
-func handlerGet(directiveStr, rootStr string) (hnd handlerType, err error) {
-	var c *caddy.Controller
-	var mids []httpserver.Middleware
-	var cfg *httpserver.SiteConfig
-	var ok bool
-	var midLen int
+func TestCGI_ServeHTTP(t *testing.T) {
+	testSetup := []struct {
+		name         string
+		cgi          CGI
+		uri          string
+		statusCode   int
+		responseBody string
+	}{
+		{
+			name: "Successful CGI request",
+			cgi: CGI{
+				Executable: "test/example",
+				ScriptName: "/foo.cgi",
+				Args:       []string{"arg1", "arg2"},
+				Envs:       []string{"CGI_GLOBAL=whatever"},
+			},
+			uri:        "/foo.cgi/some/path?x=y",
+			statusCode: 200,
+			responseBody: `PATH_INFO [/some/path]
+CGI_GLOBAL [whatever]
+Arg 1 [arg1]
+QUERY_STRING [x=y]
+REMOTE_USER []
+HTTP_TOKEN_CLAIM_USER []
+CGI_LOCAL is unset`,
+		},
+		{
+			name: "Invalid script",
+			cgi: CGI{
+				Executable: "test/example2",
+			},
+			uri:          "/whatever",
+			statusCode:   500,
+			responseBody: "",
+		},
+		{
+			name: "Inspect",
+			cgi: CGI{
+				Executable: "test/example",
+				ScriptName: "/foo.cgi",
+				Args:       []string{"arg1", "arg2"},
+				Envs:       []string{"some=thing"},
+				Inspect:    true,
+			},
+			uri:        "/foo.cgi/some/path?x=y",
+			statusCode: 200,
+			responseBody: `CGI for Caddy inspection page
 
-	c = caddy.NewTestController("http", directiveStr)
-	cfg = httpserver.GetConfig(c)
-	cfg.Root = rootStr
-	err = configureServer(c, cfg)
-	if err == nil {
-		mids = cfg.Middleware()
-		midLen = len(mids)
-		if midLen > 0 {
-			hnd, ok = mids[0](httpserver.EmptyNext).(handlerType)
-			if ok {
-				// .. success
-			} else {
-				err = fmt.Errorf("expected middleware handler to be CGI handler")
-			}
-		} else {
-			err = fmt.Errorf("no middlewares present")
-		}
+Executable .................... test/example
+  Arg 1 ....................... arg1
+  Arg 2 ....................... arg2
+Root .......................... /
+Dir ........................... 
+Environment
+  PATH_INFO ................... /some/path
+  REMOTE_USER ................. 
+  SCRIPT_FILENAME ............. test/example
+  SCRIPT_NAME ................. /foo.cgi
+  some ........................ thing
+Inherited environment`,
+		},
 	}
-	return
-}
 
-func TestServe(t *testing.T) {
-	var err error
-	var code int
-	var hnd handlerType
-	var srv *httptest.Server
-	directiveList := []string{
-		`cgi /servertime {.}/test/example`,
-		`cgi {
-  match /servertime
-  except /servertime/1934/*/*
-  exec {.}/test/example --example
-  env CGI_GLOBAL=12
-  empty_env CGI_LOCAL
-}`,
-	}
+	for _, testCase := range testSetup {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/foo.cgi/some/path?x=y", nil)
+			repl := caddy.NewReplacer()
+			req = req.WithContext(context.WithValue(req.Context(), caddy.ReplacerCtxKey, repl))
 
-	requestList := []string{
-		"/servertime",
-		"/servertime/1930/05/11?name=Edsger%20W.%20Dijkstra",
-		"/servertime/1934/02/15?name=Niklaus%20Wirth",
-		"/example.txt",
-	}
-
-	expectStr := `=== Directive 0 ===
-cgi /servertime {.}/test/example
---- Request /servertime ---
-code [0], error [example error message]
-PATH_INFO []
-CGI_GLOBAL []
-Arg 1 []
-QUERY_STRING []
-HTTP_TOKEN_CLAIM_USER [quixote]
-CGI_LOCAL is unset
---- Request /servertime/1930/05/11?name=Edsger%20W.%20Dijkstra ---
-code [0], error [example error message]
-PATH_INFO [/1930/05/11]
-CGI_GLOBAL []
-Arg 1 []
-QUERY_STRING [name=Edsger%20W.%20Dijkstra]
-HTTP_TOKEN_CLAIM_USER [quixote]
-CGI_LOCAL is unset
---- Request /servertime/1934/02/15?name=Niklaus%20Wirth ---
-code [0], error [example error message]
-PATH_INFO [/1934/02/15]
-CGI_GLOBAL []
-Arg 1 []
-QUERY_STRING [name=Niklaus%20Wirth]
-HTTP_TOKEN_CLAIM_USER [quixote]
-CGI_LOCAL is unset
---- Request /example.txt ---
-=== Directive 1 ===
-cgi {
-  match /servertime
-  except /servertime/1934/*/*
-  exec {.}/test/example --example
-  env CGI_GLOBAL=12
-  empty_env CGI_LOCAL
-}
---- Request /servertime ---
-code [0], error [example error message]
-PATH_INFO []
-CGI_GLOBAL [12]
-Arg 1 [--example]
-QUERY_STRING []
-HTTP_TOKEN_CLAIM_USER [quixote]
-CGI_LOCAL is set to []
---- Request /servertime/1930/05/11?name=Edsger%20W.%20Dijkstra ---
-code [0], error [example error message]
-PATH_INFO [/1930/05/11]
-CGI_GLOBAL [12]
-Arg 1 [--example]
-QUERY_STRING [name=Edsger%20W.%20Dijkstra]
-HTTP_TOKEN_CLAIM_USER [quixote]
-CGI_LOCAL is set to []
---- Request /servertime/1934/02/15?name=Niklaus%20Wirth ---
---- Request /example.txt ---
-`
-	// Testing the ServeHTTP method requires OS-specific CGI scripts, because a
-	// system call is made to respond to the request.
-	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
-		var buf bytes.Buffer
-		for dirJ := 0; dirJ < len(directiveList) && err == nil; dirJ++ {
-			hnd, err = handlerGet(directiveList[dirJ], "./test")
-			if err == nil {
-				srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					r.Header.Set("Token-Claim-User", "quixote")
-					r.Header.Set("Token-Claim-Language", "en-GB")
-					r.Header.Add("Token-Claim-Language", "en-AU")
-					code, err = hnd.ServeHTTP(w, r)
-					if err != nil {
-						fmt.Fprintf(&buf, "code [%d], error [%s]\n", code, err)
-					}
-				}))
-				fmt.Fprintf(&buf, "=== Directive %d ===\n%s\n", dirJ, directiveList[dirJ])
-				for reqJ := 0; reqJ < len(requestList) && err == nil; reqJ++ {
-					var res *http.Response
-					fmt.Fprintf(&buf, "--- Request %s ---\n", requestList[reqJ])
-					res, err = http.Get(srv.URL + requestList[reqJ])
-					if err == nil {
-						_, err = buf.ReadFrom(res.Body)
-						res.Body.Close()
-					}
-				}
-				srv.Close()
+			if err := testCase.cgi.ServeHTTP(res, req, NoOpNextHandler{}); err != nil {
+				t.Fatalf("Cannot serve http: %v", err)
 			}
-		}
-		if err == nil {
-			gotStr := buf.String()
-			if expectStr != gotStr {
-				err = errorf("expected %s, got %s\n", expectStr, gotStr)
+
+			if res.Code != testCase.statusCode {
+				t.Errorf("Unexpected statusCode %d. Expected %d.", res.Code, testCase.statusCode)
 			}
-		}
-		if err != nil {
-			t.Fatalf("%s", err)
-		}
+
+			bodyString := strings.TrimSpace(res.Body.String())
+			if bodyString != testCase.responseBody {
+				t.Errorf("Unexpected body\n========== Got ==========\n%s\n========== Wanted ==========\n%s", bodyString, testCase.responseBody)
+			}
+		})
 	}
 }
 
-func checkEnv(envStr string) (err error) {
-	// envStr is reported by CGI program invoked with pass_all_env. It looks like
-	// "key1=val1\nkey2=val2..." Place keys into map and make sure all actual
-	// environment variables are included in this map. Some environment values get
-	// legitimately changed for spawned executable, so verify keys only.
-	list := strings.Split(envStr, "\n")
-	mp := make(map[string]bool)
-	for _, pr := range list {
-		pos := strings.Index(pr, "=")
-		if pos > 0 {
-			mp[pr[:pos]] = true
-		}
-	}
-	actualList := os.Environ()
-	actualLen := len(actualList)
-	for j := 0; j < actualLen && err == nil; j++ {
-		actualStr := actualList[j]
-		pos := strings.Index(actualStr, "=")
-		if pos > 0 {
-			k := actualStr[:pos]
-			_, ok := mp[k]
-			if !ok {
-				// err = fmt.Errorf("environment key \"%s\" not found in CGI environment", k)
-				fmt.Printf("environment key \"%s\" not found in CGI environment\n", k)
-			}
-		}
-	}
-	return
-}
-
-func TestDir(t *testing.T) {
-	var err error
-	var code int
-	var rsp string
-	var hnd handlerType
-	var srv *httptest.Server
-	request := `/tmpdir`
-	directive := `cgi {
-match /tmpdir
-exec {.}/test/showdir
-dir /tmp
-}`
-
-	// Testing the ServeHTTP method requires OS-specific CGI scripts, because a
-	// system call is made to respond to the request.
-	if runtime.GOOS == "linux" {
-		var buf bytes.Buffer
-		hnd, err = handlerGet(directive, "./test")
-		if err == nil {
-			srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				code, err = hnd.ServeHTTP(w, r)
-				if err != nil {
-					fmt.Fprintf(&buf, "code [%d], error [%s]\n", code, err)
-				}
-			}))
-			var res *http.Response
-			res, err = http.Get(srv.URL + request)
-			if err == nil {
-				_, err = buf.ReadFrom(res.Body)
-				res.Body.Close()
-			}
-			srv.Close()
-		}
-		if err == nil {
-			rsp = strings.TrimSpace(buf.String())
-			if "/tmp" != rsp {
-				err = fmt.Errorf("expecting \"/tmp\", got \"%s\"", rsp)
-			}
-		}
-		if err != nil {
-			t.Fatalf("%s", err)
-		}
-	}
-}
-
-func TestPassAll(t *testing.T) {
-	var err error
-	var code int
-	var hnd handlerType
-	var srv *httptest.Server
-	request := `/full`
-	directive := `cgi {
-match /full
-exec {.}/test/fullenv
-pass_all_env
-}`
-
-	// Testing the ServeHTTP method requires OS-specific CGI scripts, because a
-	// system call is made to respond to the request.
-	if runtime.GOOS == "linux" {
-		var buf bytes.Buffer
-		hnd, err = handlerGet(directive, "./test")
-		if err == nil {
-			srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				code, err = hnd.ServeHTTP(w, r)
-				if err != nil {
-					fmt.Fprintf(&buf, "code [%d], error [%s]\n", code, err)
-				}
-			}))
-			fmt.Fprintf(&buf, "=== Directive ===\n%s\n", directive)
-			var res *http.Response
-			fmt.Fprintf(&buf, "--- Request %s ---\n", request)
-			res, err = http.Get(srv.URL + request)
-			if err == nil {
-				_, err = buf.ReadFrom(res.Body)
-				res.Body.Close()
-			}
-			srv.Close()
-		}
-		if err == nil {
-			err = checkEnv(buf.String())
-		}
-		if err != nil {
-			t.Fatalf("%s", err)
-		}
-	}
-}
-
-func TestMatches(t *testing.T) {
-	var ok bool
-	var prefixStr, suffixStr string
-	// [request, pattern, expected success:1/expected error:0, prefix, suffix]
-	list := [][]string{
-		{"/foo/bar/baz", "/foo", "1", "/foo", "/bar/baz"},
-		{"/foo/bar/baz", "/foo/*/baz", "1", "/foo/bar/baz", ""},
-		{"/foo/bar/baz", "/foo/bar", "1", "/foo/bar", "/baz"},
-		{"/foo/bar/baz", "foo/bar", "0", "", ""},
-	}
-
-	for _, rec := range list {
-		ok, prefixStr, suffixStr = match(rec[0], []string{rec[1]})
-		if ok {
-			if rec[2] == "0" || rec[3] != prefixStr || rec[4] != suffixStr {
-				t.Fatalf("expected mismatch for \"%s\" and \"%s\"", rec[0], rec[1])
-			}
-		} else {
-			if rec[2] == "1" {
-				t.Fatalf("expected match for \"%s\" and \"%s\"", rec[0], rec[1])
-			}
-		}
-	}
-}
-
-func TestExceptions(t *testing.T) {
-	// [request, except pattern, expected success:1/expected error:0]
-	list := [][]string{
-		{"/foo/bar/baz.png", "/*/*/*.png", "1"},
-		{"/foo/bar/baz.png", "/foo/*/baz*", "1"},
-		{"/foo/bar/baz.png", "/foo/bar/baz.jpg", "0"},
-		{"/foo/bar/baz.png", "foo/bar", "0"},
-	}
-
-	for _, rec := range list {
-		ok := excluded(rec[0], []string{rec[1]})
-		if ok && rec[2] == "0" {
-			t.Fatalf("unexpected exception for \"%s\" and \"%s\"", rec[0], rec[1])
-		} else if !ok && rec[2] == "1" {
-			t.Fatalf("expected exception for \"%s\" and \"%s\"", rec[0], rec[1])
-		}
-	}
-}
-
-func TestInspect(t *testing.T) {
-	var err error
-	var hnd handlerType
-	var srv *httptest.Server
-	var buf bytes.Buffer
-
-	block := `cgi {
-  match /foo/*
-  exec bar --baz --quux
-  pass_env HOME
-  env VERY_LONG_KEY_INTENDED_TO_TEST_INSPECTION_REPORT=foo
+func TestCGI_UnmarshalCaddyfile(t *testing.T) {
+	content := `cgi /some/file a b c d 1 {
+  dir /somewhere
+  script_name /my.cgi
+  env foo=bar what=ever
+  pass_env some_env other_env
+  pass_all_env
   inspect
 }`
+	d := caddyfile.NewTestDispenser(content)
+	var c CGI
+	if err := c.UnmarshalCaddyfile(d); err != nil {
+		t.Fatalf("Cannot parse caddyfile: %v", err)
+	}
 
-	hnd, err = handlerGet(block, "./test")
-	if err == nil {
-		srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			_, hndErr := hnd.ServeHTTP(w, r)
-			if err == nil && hndErr != nil {
-				err = hndErr
-			}
-		}))
-		var res *http.Response
-		res, err = http.Get(srv.URL + "/foo/bar.tcl")
-		if err == nil {
-			_, err = buf.ReadFrom(res.Body)
-			if err == nil {
-				str := buf.String()
-				if !strings.Contains(str, "CGI for Caddy") {
-					err = fmt.Errorf("unexpected response for \"inspect\" request")
-				}
-			}
-			res.Body.Close()
-		}
-		srv.Close()
+	expected := CGI{
+		Executable:       "/some/file",
+		WorkingDirectory: "/somewhere",
+		ScriptName:       "/my.cgi",
+		Args:             []string{"a", "b", "c", "d", "1"},
+		Envs:             []string{"foo=bar", "what=ever"},
+		PassEnvs:         []string{"some_env", "other_env"},
+		PassAll:          true,
+		Inspect:          true,
 	}
-	if err != nil {
-		t.Fatalf("%s", err)
+
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatal("Parsing yielded invalid result.")
 	}
+}
+
+type NoOpNextHandler struct{}
+
+func (n NoOpNextHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
+	// Do Nothing
+	return nil
 }
